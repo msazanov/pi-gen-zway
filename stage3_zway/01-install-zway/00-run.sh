28839b92d095e7b7fdf1772405a3531efa3f3cff
Marat Sazanov <msazanov@gmail.com>
2023-12-29 19:41:53 +0400

    Update 00-run.sh

--- stage3_zway/01-install-zway/00-run.sh
+++ stage3_zway/01-install-zway/00-run.sh
@@ -1,30 +1,29 @@
 #!/bin/bash -e
 
-# Отладка: Вывод информации о добавлении репозитория
-echo "Добавление файла репозитория z-wave-me.list в ${ROOTFS_DIR}/etc/apt/sources.list.d/"
+# Debugging: Display information about adding the repository
+echo "Adding z-wave-me.list repository file to ${ROOTFS_DIR}/etc/apt/sources.list.d/"
 install -m 644 files/z-wave-me.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/z-wave-me.list"
 
-# Отладка: Проверка содержимого файла репозитория
-echo "Содержимое файла репозитория z-wave-me.list:"
-cat "${ROOTFS_DIR}/etc/apt/sources.list.d/z-wave-me.list"
+# Debugging: Check the contents of the repository file
+#echo "Contents of the z-wave-me.list repository file:"
+#cat "${ROOTFS_DIR}/etc/apt/sources.list.d/z-wave-me.list"
 
-# Отладка: Добавление и проверка ключа GPG
-echo "Добавление ключа GPG"
+# Debugging: Adding and verifying the GPG key
+echo "Adding GPG key"
 cat files/z-wave-me.gpg.key | gpg --dearmor > "${STAGE_WORK_DIR}/z-wave-me.gpg"
 install -m 644 "${STAGE_WORK_DIR}/z-wave-me.gpg" "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
 
-echo "Проверка добавленного ключа GPG в ${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
-ls -l "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
+#echo "Checking the added GPG key in ${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
+#ls -l "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
 
-# Отладка: Обновление списка пакетов в chroot
-echo "Добавляем потдержку ахитектуры armhf"
+echo "Adding support for armhf architecture"
 on_chroot << EOF
 dpkg --add-architecture armhf
 apt-get update
 EOF
 
-echo "Добавляем флаг no_connection для ZBW"
+echo "Adding no_connection flag for ZBW"
 on_chroot << EOF
 mkdir -p /etc/zbw/flags/
 touch /etc/zbw/flags/no_connection
@@ -34,7 +33,3 @@ EOF
 rm -f "${ROOTFS_DIR}"/etc/motd
 rm -f "${ROOTFS_DIR}"/etc/update-motd.d/10-uname
 install -m 755 files/motd-RaZberry "${ROOTFS_DIR}"/etc/update-motd.d/10-razberry
-
-
-# Отладка завершена
-echo "Скрипт завершил выполнение"
